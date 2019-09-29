771073fbfc8bd314e2ecbf4fc317bc798ed60f03
martinhug <mhug@hsr.ch>
2019-09-29 16:55:44 +0200

    div anpassungen push für david

--- restconf_configurator.py
+++ restconf_configurator.py
@@ -4,8 +4,6 @@ from typing import List
 import requests
 import yaml
 
-import restconf_helpers
-
 requests.packages.urllib3.disable_warnings()
 
 logger = logging.getLogger('restconf.example')
@@ -16,8 +14,8 @@ HEADERS = {
 }
 
 
-def load_devices() -> List[dict]:
-    with open('device_infos.yaml', 'r') as host_file:
+def load_yaml(filename) -> List[dict]:
+    with open(filename , 'r') as host_file:
         hosts = yaml.load(host_file.read(), Loader=yaml.FullLoader)
         return hosts
 
@@ -29,6 +27,22 @@ def init_logger():
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
     ch.setFormatter(formatter)
     _logger.addHandler(ch)
+
+from jinja2 import Environment, FileSystemLoader
+
+
+def load_template():
+    # Load Jinja2 template
+    env = Environment(loader=FileSystemLoader('./'), trim_blocks=True, lstrip_blocks=True)
+    template = env.get_template('router_config.xml')
+    print(template)
+    return template
+
+def render_template(template, hosts):
+    template.render(hosts)
+    print(hosts)
+
+
 
 
 def patch_interface_config(host, xml_payload):
@@ -66,17 +80,36 @@ def get_xml(filename):
 
 
 def main():
-    devices = load_devices()
+    print("Load Device Infos")
+    devices = load_yaml('device_infos.yaml')
     for device in devices:
-        logger.info(f'Configuring Interfaces for device {device["connection_address"]}')
-        response_interface = patch_interface_config(device, get_xml("router_interface_config.xml"))
-        print(response_interface)
-        logger.info(f'Configuring OSPF for device {device["connection_address"]}')
-        response_ospf = patch_ospf_config(device, get_xml("router_ospf_config.xml"))
-        print(response_ospf)
-        logger.info(f'Configuring BGP for device {device["connection_address"]}')
-        response_bgp = patch_bgp_config(device, get_xml("router_bgp_config.xml"))
-        print(response_bgp)
+        logger.info('Getting information for device {device')
+        print(device['username'])
+        template = load_template()
+        template.render(device[''])
+        render_template(template, device)
+        print(template.stream().dump('hello.html'))
+
+
+    loopback = load_yaml('loopback_infos.yaml')
+    print(loopback)
+    ospf = load_yaml('ospf_infos.yaml')
+    print(ospf)
+    bgp = load_yaml('bgp_infos.yaml')
+    print(bgp)
+    # template = load_template()
+    # template.render()
+    # render_template(template, devices)
+    # # for device in devices:
+    #     logger.info(f'Configuring Interfaces for device {device["connection_address"]}')
+    #     response_interface = patch_interface_config(device, get_xml("router_interface_config.xml"))
+    #     print(response_interface)
+    #     logger.info(f'Configuring OSPF for device {device["connection_address"]}')
+    #     response_ospf = patch_ospf_config(device, get_xml("router_ospf_config.xml"))
+    #     print(response_ospf)
+    #     logger.info(f'Configuring BGP for device {device["connection_address"]}')
+    #     response_bgp = patch_bgp_config(device, get_xml("router_bgp_config.xml"))
+    #     print(response_bgp)
 
 
 if __name__ == '__main__':
